97db475a0b983a96b38cc3520ed26fecde6d4a92
hsgang <hsgangmain@naver.com>
2023-01-30 11:50:19 +0900

    add custom.xml

--- custom/testsuite.h
+++ custom/testsuite.h
@@ -12,17 +12,17 @@ extern "C" {
 
 #ifndef MAVLINK_TEST_ALL
 #define MAVLINK_TEST_ALL
-static void mavlink_test_ardupilotmega(uint8_t, uint8_t, mavlink_message_t *last_msg);
+
 static void mavlink_test_custom(uint8_t, uint8_t, mavlink_message_t *last_msg);
 
 static void mavlink_test_all(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
-    mavlink_test_ardupilotmega(system_id, component_id, last_msg);
+
     mavlink_test_custom(system_id, component_id, last_msg);
 }
 #endif
 
-#include "../ardupilotmega/testsuite.h"
+
 
 
 static void mavlink_test_atmospheric_value(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
--- icarous/icarous.h
+++ icarous/icarous.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ICAROUS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ICAROUS_XML_HASH -1369357772951456850
+#define MAVLINK_ICAROUS_XML_HASH -7257553604971087153
 
 #ifdef __cplusplus
 extern "C" {
